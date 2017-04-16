d00a1a31e117d42ca8ee7ff724b0a689e00120ed
Schinzel <henrik@schinzel.se>
2017-04-16 17:06:39 +0200

    Doc

---
+++ src/main/java/com/atexpose/atexpose/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains a set of interfaces that implements various isolated functionalities of @Expose.
+ *
+ * Created by schinzel on 2017-04-16.
+ */
+package com.atexpose.atexpose;
