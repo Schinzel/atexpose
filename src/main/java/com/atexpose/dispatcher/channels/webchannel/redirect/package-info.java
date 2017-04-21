9718c058cd910ca8d491871b4c1605d297667300
Henrik Schinzel <henrik.schinzel@kollektiva.se>
2017-04-21 06:35:37 +0200

    Doc

---
+++ src/main/java/com/atexpose/dispatcher/channels/webchannel/redirect/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The purpose of this package is to handle http-redirects. I.e. 302s.
+ * Created by schinzel on 2017-04-21.
+ */
+package com.atexpose.dispatcher.channels.webchannel.redirect;
