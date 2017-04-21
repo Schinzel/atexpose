18eb5d5dc979c45c49392585f3c30d457af3293c
Henrik Schinzel <henrik.schinzel@kollektiva.se>
2017-04-21 11:01:40 +0200

    Test added

--- src/test/java/com/atexpose/dispatcher/channels/webchannel/redirect/RedirectsTest.java
+++ src/test/java/com/atexpose/dispatcher/channels/webchannel/redirect/RedirectsTest.java
@@ -21,5 +21,17 @@ public class RedirectsTest {
         Assert.assertEquals("https://www.schinzel.io/q/r/s/dest.html?k1=ŹźŻż&k2=سش&k3=ДЂ", redirects.getRedirects(uri).toString());
     }
 
+    @Test
+    public void shouldRedirect_noRedidect() throws Exception {
+        ImmutableList<IRedirect> myList = new ImmutableList.Builder<IRedirect>()
+                .add(HttpsRedirect.create())
+                .add(HostRedirect.create("example.com", "www.example.com"))
+                .add(HostRedirect.create("www.example.com", "www.schinzel.io"))
+                .add(FileRedirect.create("a/b/c/source.html", "q/r/s/dest.html"))
+                .build();
+        Redirects redirects = new Redirects(myList);
+        URI uri = new URI("https://www.google.com");
+        Assert.assertFalse(redirects.shouldRedirect(uri));
+    }
 
 }
