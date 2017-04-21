95ed02a7c75e0bfa563fc03b0c669569e3c5e7b0
Henrik Schinzel <henrik.schinzel@kollektiva.se>
2017-04-21 09:32:44 +0200

    Condensed tests

--- src/test/java/com/atexpose/dispatcher/channels/webchannel/redirect/HostRedirectTest.java
+++ src/test/java/com/atexpose/dispatcher/channels/webchannel/redirect/HostRedirectTest.java
@@ -20,103 +20,49 @@ public class HostRedirectTest {
 
 
     @Test
-    public void shouldRedirect_root_to_www() throws Exception {
+    public void shouldAndGetRedirect_root_to_www() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
                 .from("example.com")
                 .to("www.example.com")
                 .build();
         URI uri = new URIBuilder("http://example.com").build();
         Assert.assertTrue(hostRedirect.shouldRedirect(uri));
+        Assert.assertEquals("http://www.example.com/", hostRedirect.getRedirect(uri).toString());
     }
 
 
     @Test
-    public void shouldRedirect_another_domain() throws Exception {
+    public void shouldAndGetRedirect_another_domain() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
                 .from("www.example.com")
                 .to("www.schinzel.io")
                 .build();
         URI uri = new URIBuilder("http://www.example.com").build();
         Assert.assertTrue(hostRedirect.shouldRedirect(uri));
+        Assert.assertEquals("http://www.schinzel.io/", hostRedirect.getRedirect(uri).toString());
     }
 
 
     @Test
-    public void shouldRedirect_subdomain() throws Exception {
+    public void shouldAndGetRedirect_subdomain() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
                 .from("sub1.example.com")
                 .to("sub2.example.com")
                 .build();
         URI uri = new URIBuilder("http://sub1.example.com").build();
         Assert.assertTrue(hostRedirect.shouldRedirect(uri));
+        Assert.assertEquals("http://sub2.example.com/", hostRedirect.getRedirect(uri).toString());
     }
 
 
     @Test
-    public void shouldRedirect_https() throws Exception {
+    public void shouldAndGetRedirect_https() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
                 .from("sub1.example.com")
                 .to("sub2.example.io")
                 .build();
         URI uri = new URIBuilder("https://sub1.example.com").build();
         Assert.assertTrue(hostRedirect.shouldRedirect(uri));
-    }
-
-
-    /**
-     * Testing with query string with polish, persian and cyrillic chars.
-     */
-    @Test
-    public void shouldRedirect_with_querystring() throws Exception {
-        HostRedirect hostRedirect = HostRedirect.builder()
-                .from("www.example.com")
-                .to("www.schinzel.io")
-                .build();
-        URI uri = new URIBuilder("http://www.example.com?k1=ŹźŻż&k2=سش&k3=ДЂ").build();
-        Assert.assertTrue(hostRedirect.shouldRedirect(uri));
-    }
-
-
-    @Test
-    public void getRedirect_root_to_www() throws Exception {
-        HostRedirect hostRedirect = HostRedirect.builder()
-                .from("example.com")
-                .to("www.example.com")
-                .build();
-        URI uri = new URIBuilder("http://example.com").build();
-        Assert.assertEquals("http://www.example.com/", hostRedirect.getRedirect(uri).toString());
-    }
-
-
-    @Test
-    public void getRedirect_another_domain() throws Exception {
-        HostRedirect hostRedirect = HostRedirect.builder()
-                .from("www.example.com")
-                .to("www.schinzel.io")
-                .build();
-        URI uri = new URIBuilder("http://www.example.com").build();
-        Assert.assertEquals("http://www.schinzel.io/", hostRedirect.getRedirect(uri).toString());
-    }
-
-
-    @Test
-    public void getRedirect_subdomain() throws Exception {
-        HostRedirect hostRedirect = HostRedirect.builder()
-                .from("sub1.example.com")
-                .to("sub2.example.io")
-                .build();
-        URI uri = new URIBuilder("http://sub1.example.com").build();
-        Assert.assertEquals("http://sub2.example.io/", hostRedirect.getRedirect(uri).toString());
-    }
-
-
-    @Test
-    public void getRedirect_https() throws Exception {
-        HostRedirect hostRedirect = HostRedirect.builder()
-                .from("sub1.example.com")
-                .to("sub2.example.io")
-                .build();
-        URI uri = new URIBuilder("https://sub1.example.com").build();
         Assert.assertEquals("https://sub2.example.io/", hostRedirect.getRedirect(uri).toString());
     }
 
@@ -125,13 +71,15 @@ public class HostRedirectTest {
      * Testing with query string with polish, persian and cyrillic chars.
      */
     @Test
-    public void getRedirect_with_querystring() throws Exception {
+    public void shouldAndGetRedirect_with_querystring() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
                 .from("www.example.com")
                 .to("www.schinzel.io")
                 .build();
         URI uri = new URIBuilder("http://www.example.com?k1=ŹźŻż&k2=سش&k3=ДЂ").build();
+        Assert.assertTrue(hostRedirect.shouldRedirect(uri));
         Assert.assertEquals("http://www.schinzel.io/?k1=ŹźŻż&k2=سش&k3=ДЂ", hostRedirect.getRedirect(uri).toString());
     }
+
 
 }
