d902ae1a6775aad6545f34cfa4cf152b65fc19e4
Henrik Schinzel <henrik.schinzel@kollektiva.se>
2017-04-21 09:14:45 +0200

    More tests

--- src/test/java/com/atexpose/dispatcher/channels/webchannel/redirect/HostRedirectTest.java
+++ src/test/java/com/atexpose/dispatcher/channels/webchannel/redirect/HostRedirectTest.java
@@ -7,6 +7,18 @@ import org.junit.Test;
 import java.net.URI;
 
 public class HostRedirectTest {
+
+    @Test
+    public void shouldRedirect_should_not_redirect() throws Exception {
+        HostRedirect hostRedirect = HostRedirect.builder()
+                .from("www.example.com")
+                .to("www.example.com")
+                .build();
+        URI uri = new URIBuilder("http://example.com").build();
+        Assert.assertFalse(hostRedirect.shouldRedirect(uri));
+    }
+
+
     @Test
     public void shouldRedirect_root_to_www() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
@@ -40,6 +52,20 @@ public class HostRedirectTest {
     }
 
 
+    /**
+     * Testing with query string with polish, persian and cyrillic chars.
+     */
+    @Test
+    public void shouldRedirect_with_querystring() throws Exception {
+        HostRedirect hostRedirect = HostRedirect.builder()
+                .from("www.example.com")
+                .to("www.schinzel.io")
+                .build();
+        URI uri = new URIBuilder("http://www.example.com?k1=ŹźŻż&k2=سش&k3=ДЂ").build();
+        Assert.assertTrue(hostRedirect.shouldRedirect(uri));
+    }
+
+
     @Test
     public void getRedirect_root_to_www() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
@@ -63,7 +89,7 @@ public class HostRedirectTest {
 
 
     @Test
-    public void getdRedirect_subdomain() throws Exception {
+    public void getRedirect_subdomain() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
                 .from("sub1.example.com")
                 .to("sub2.example.io")
@@ -77,7 +103,7 @@ public class HostRedirectTest {
      * Testing with query string with polish, persian and cyrillic chars.
      */
     @Test
-    public void getdRedirect_with_querystring() throws Exception {
+    public void getRedirect_with_querystring() throws Exception {
         HostRedirect hostRedirect = HostRedirect.builder()
                 .from("www.example.com")
                 .to("www.schinzel.io")
