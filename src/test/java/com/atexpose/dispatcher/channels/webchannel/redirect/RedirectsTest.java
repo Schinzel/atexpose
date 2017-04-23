0bfd2ea06ec344895d5acad0ff46890b9e4f904f
Schinzel <henrik@schinzel.se>
2017-04-23 07:33:41 +0200

    Test added

--- src/test/java/com/atexpose/dispatcher/channels/webchannel/redirect/RedirectsTest.java
+++ src/test/java/com/atexpose/dispatcher/channels/webchannel/redirect/RedirectsTest.java
@@ -7,7 +7,7 @@ import java.net.URI;
 
 public class RedirectsTest {
     @Test
-    public void shouldAndGetRedirect() throws Exception {
+    public void shouldRedirectAndGetNewLocation() throws Exception {
         Redirects redirects = Redirects.getBuilder()
                 .setHttpsRedirect()
                 .addHostRedirect("example.com", "www.example.com")
@@ -30,6 +30,36 @@ public class RedirectsTest {
                 .build();
         URI uri = new URI("https://www.google.com");
         Assert.assertFalse(redirects.shouldRedirect(uri));
+    }
+
+
+    @Test
+    public void shouldRedirectAndGetNewLocation_failWhale() throws Exception {
+        Redirects redirects = Redirects.getBuilder()
+                .setHttpsRedirect()
+                .setFailWhaleRedirect("bapp.html")
+                .addHostRedirect("example.com", "www.example.com")
+                .addHostRedirect("www.example.com", "www.schinzel.io")
+                .addFileRedirect("a/b/c/source.html", "q/r/s/dest.html")
+                .build();
+        URI uri = new URI("https://www.schinzel.io");
+        //The only redirect clause that hits is the fail whale.
+        Assert.assertTrue(redirects.shouldRedirect(uri));
+        Assert.assertEquals("https://www.schinzel.io/bapp.html", redirects.getNewLocation(uri).toString());
+    }
+
+
+    @Test
+    public void getNewLocation_failWhale_multipleRedirectsHits() throws Exception {
+        Redirects redirects = Redirects.getBuilder()
+                .setFailWhaleRedirect("bapp.html")
+                .addHostRedirect("example.com", "www.example.com")
+                .addHostRedirect("www.example.com", "www.schinzel.io")
+                .addFileRedirect("a/b/c/source.html", "q/r/s/dest.html")
+                .setHttpsRedirect()
+                .build();
+        URI uri = new URI("http://example.com/a/b/c/source.html");
+        Assert.assertEquals("https://www.schinzel.io/bapp.html", redirects.getNewLocation(uri).toString());
     }
 
 }
