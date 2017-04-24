13a371c2be1d8998df80dfe7ce8b799b135e9b80
Henrik Schinzel <henrik.schinzel@kollektiva.se>
2017-04-24 10:59:03 +0200

    Broke up tests

--- src/test/java/com/atexpose/WebServerRedirectTest.java
+++ src/test/java/com/atexpose/WebServerRedirectTest.java
@@ -3,6 +3,8 @@ package com.atexpose;
 import com.atexpose.dispatcher.Dispatcher;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -15,58 +17,75 @@ import static org.junit.Assert.assertEquals;
  * Created by Schinzel on 2017-04-24.
  */
 public class WebServerRedirectTest {
+    Dispatcher mWebServer;
 
 
-    @Test
-    public void testWebServerRedirects() throws IOException {
-        int statusCode;
-        String location;
-        Connection.Response response;
-        Dispatcher webServer = AtExpose.create().getWebServerBuilder()
+    @Before
+    public void before() {
+        mWebServer = AtExpose.create().getWebServerBuilder()
                 .numberOfThreads(5)
                 .addFileRedirect("src.html", "dest.html")
                 .addFileRedirect("dir1/dir2/src.html", "dirdest/dest.html")
                 .startWebServer();
+    }
+
+
+    @After
+    public void after() {
+        mWebServer.shutdown();
+    }
+
+
+    @Test
+    public void testRedirect_pageInRoot() throws Exception {
         //Basic test
-        response = Jsoup
+        Connection.Response response = Jsoup
                 .connect("http://127.0.0.1:5555/src.html")
                 .method(Connection.Method.GET)
                 .followRedirects(false)
                 .execute();
-        statusCode = response.statusCode();
-        location = response.header("Location");
-        assertEquals(302, statusCode);
-        assertEquals("http://127.0.0.1:5555/dest.html", location);
-        //Test with dirs
-        response = Jsoup
+        assertEquals(302, response.statusCode());
+        assertEquals("http://127.0.0.1:5555/dest.html", response.header("Location"));
+    }
+
+
+    @Test
+    public void testRedirect_pageInSubDir() throws Exception {
+        //Basic test
+        Connection.Response response = Jsoup
                 .connect("http://127.0.0.1:5555/dir1/dir2/src.html")
                 .method(Connection.Method.GET)
                 .followRedirects(false)
                 .execute();
-        statusCode = response.statusCode();
-        location = response.header("Location");
-        assertEquals(302, statusCode);
-        assertEquals("http://127.0.0.1:5555/dirdest/dest.html", location);
-        //Test that query strings are passed on
-        response = Jsoup
+        assertEquals(302, response.statusCode());
+        assertEquals("http://127.0.0.1:5555/dirdest/dest.html", response.header("Location"));
+    }
+
+
+    @Test
+    public void testRedirect_pageInRoot_withQueryString() throws Exception {
+        //Basic test
+        Connection.Response response = Jsoup
                 .connect("http://127.0.0.1:5555/src.html?key1=val1")
                 .method(Connection.Method.GET)
                 .followRedirects(false)
                 .execute();
-        statusCode = response.statusCode();
-        location = response.header("Location");
-        assertEquals(302, statusCode);
-        assertEquals("http://127.0.0.1:5555/dest.html?key1=val1", location);
+        assertEquals(302, response.statusCode());
+        assertEquals("http://127.0.0.1:5555/dest.html?key1=val1", response.header("Location"));
+    }
+
+
+    @Test
+    public void testRedirect_pageInSubDir_withQueryStriing() throws IOException {
         //Test that query strings are passed on with dirs
-        response = Jsoup
+        Connection.Response response = Jsoup
                 .connect("http://127.0.0.1:5555/dir1/dir2/src.html?key2=val2")
                 .method(Connection.Method.GET)
                 .followRedirects(false)
                 .execute();
-        statusCode = response.statusCode();
-        location = response.header("Location");
-        assertEquals(302, statusCode);
-        assertEquals("http://127.0.0.1:5555/dirdest/dest.html?key2=val2", location);
-        webServer.shutdown();
+        assertEquals(302, response.statusCode());
+        assertEquals("http://127.0.0.1:5555/dirdest/dest.html?key2=val2", response.header("Location"));
     }
+
+
 }
