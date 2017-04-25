a3de66e03b098859c129c91f5c291d4337f70e4a
Schinzel <henrik@schinzel.se>
2017-04-25 20:26:26 +0200

    Skeleton code added

---
+++ src/main/java/com/atexpose/dispatcher/parser/Request.java
@@ -0,0 +1,25 @@
+package com.atexpose.dispatcher.parser;
+
+import io.schinzel.basicutils.EmptyObjects;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * An instance of this object is an incoming request.
+ * <p>
+ * Created by schinzel on 2017-04-25.
+ */
+@Builder
+@Accessors(prefix = "m")
+public class Request {
+    @Getter @Builder.Default private String mMethodName = "";
+    @Getter @Builder.Default private List<String> mArgumentNames = EmptyObjects.EMPTY_LIST;
+    @Getter @Builder.Default private List<String> mArgumentValues = EmptyObjects.EMPTY_LIST;
+    @Getter @Builder.Default private String mFileName = "";
+    @Getter @Builder.Default private boolean mFileRequest = false;
+
+
+}
