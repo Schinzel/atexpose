25c5205b81ee22386c588e185aa89b1a9454a668
Schinzel <henrik@schinzel.io>
2017-07-02 18:12:05 +0200

    Can send

--- src/main/java/io/schinzel/REMOVE_ME.java
+++ src/main/java/io/schinzel/REMOVE_ME.java
@@ -3,8 +3,11 @@ package io.schinzel;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import io.schinzel.basicutils.RandomUtil;
 import io.schinzel.basicutils.configvar.ConfigVar;
 
 /**
@@ -17,12 +20,21 @@ public class REMOVE_ME {
         ConfigVar configVar = ConfigVar.create(".env");
         String awsAccessKey = configVar.getValue("AWS_SQS_ACCESS_KEY");
         String awsSecretKey = configVar.getValue("AWS_SQS_SECRET_KEY");
+        System.out.println("Access: '" + awsAccessKey + "'");
+        System.out.println("Secret: '" + awsSecretKey + "'");
         AWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-        //AmazonSQSClient sqs = new AmazonSQSClient(credentials);
-        AmazonSQS amazonSQS = AmazonSQSClientBuilder
+        AmazonSQS sqs = AmazonSQSClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.EU_WEST_1)
                 .build();
+        SendMessageRequest send_msg_request = new SendMessageRequest()
+                .withQueueUrl("https://sqs.eu-west-1.amazonaws.com/146535832843/my_first_queue.fifo")
+                .withMessageBody("hello world 2!!")
+                .withMessageGroupId("bappidipabpp")
+                .withMessageDeduplicationId(RandomUtil.getRandomString(10));
+        //.withDelaySeconds(5);
+        sqs.sendMessage(send_msg_request);
         //sqs.setEndpoint("https://sqs.us-east-2.amazonaws.com");
         System.out.println("All done!");
     }
