b143c90fd8807d876dbc465414649a278aa32d77
Schinzel <henrik@schinzel.io>
2017-07-02 19:01:33 +0200

    Receiver class

--- src/main/java/io/schinzel/REMOVE_ME.java
+++ src/main/java/io/schinzel/REMOVE_ME.java
@@ -7,11 +7,33 @@ import io.schinzel.basicutils.timekeeper.Timekeeper;
 /**
  * Få iväg ett meddelande till SQS
  * Kunna läsa ett meddelande
+ * Fatta group id
  */
 
 public class REMOVE_ME {
     public static void main(String[] args) {
         System.out.println("Started!");
+        testReceive();
+        System.out.println("All done!");
+    }
+
+
+    public static void testReceive() {
+        ConfigVar configVar = ConfigVar.create(".env");
+        String awsAccessKey = configVar.getValue("AWS_SQS_ACCESS_KEY");
+        String awsSecretKey = configVar.getValue("AWS_SQS_SECRET_KEY");
+        SqsReceiver sqsReceiver = SqsReceiver.builder()
+                .awsAccessKey(awsAccessKey)
+                .awsSecretKey(awsSecretKey)
+                .queueUrl("https://sqs.eu-west-1.amazonaws.com/146535832843/my_first_queue.fifo")
+                .region(Regions.EU_WEST_1)
+                .build();
+        String message = sqsReceiver.receive();
+        System.out.println("Message '" + message + "'");
+    }
+
+
+    public static void testSend() {
         ConfigVar configVar = ConfigVar.create(".env");
         String awsAccessKey = configVar.getValue("AWS_SQS_ACCESS_KEY");
         String awsSecretKey = configVar.getValue("AWS_SQS_SECRET_KEY");
@@ -29,7 +51,6 @@ public class REMOVE_ME {
         tk.stopLap().startLap("s3");
         sqsSender.send("s3");
         tk.stopLap().stop().toStr().pln();
-        System.out.println("All done!");
     }
 
 
---
+++ src/main/java/io/schinzel/SqsReceiver.java
@@ -0,0 +1,50 @@
+package io.schinzel;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * The purpose of this class is to read from an AWS SQS queue.
+ * Created by schinzel on 2017-07-02.
+ */
+public class SqsReceiver {
+    @Getter(AccessLevel.PRIVATE) private final String queueUrl;
+    @Getter(AccessLevel.PRIVATE) private final AmazonSQS sqsClient;
+
+
+    @Builder
+    private SqsReceiver(String awsAccessKey, String awsSecretKey, Regions region, String queueUrl) {
+        AWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        this.sqsClient = AmazonSQSClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+        this.queueUrl = queueUrl;
+    }
+
+
+    String receive() {
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest()
+                .withMaxNumberOfMessages(1)
+                .withQueueUrl(this.getQueueUrl());
+        List<Message> messages = this.getSqsClient()
+                .receiveMessage(receiveMessageRequest).getMessages();
+        System.out.println("Num messages" + messages);
+        Message message = messages.get(0);
+        this.getSqsClient()
+                .deleteMessage(this.getQueueUrl(), message.getReceiptHandle());
+        return message.getBody();
+    }
+}
