cb5382d8177230c28c3be123c4ab52fd5c1b6dfa
Schinzel <henrik@schinzel.io>
2017-07-03 12:27:06 +0200

    Set up intration test dir

---
+++ src/test/integration/java/com/atexpose/util/sqs/SqsSenderTest.java
@@ -0,0 +1,12 @@
+package com.atexpose.util.sqs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SqsSenderTest {
+    @Test
+    public void send_SendMessage_ReadSentMessage() {
+        assertTrue(4 == 4);
+    }
+}
