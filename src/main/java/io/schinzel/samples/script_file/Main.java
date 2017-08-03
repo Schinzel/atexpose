d04d4acf77092cba0f777c187f251d7356e681de
Schinzel <henrik@schinzel.io>
2017-08-03 07:26:02 +0200

    Polished sample

--- src/main/java/io/schinzel/samples/script_file/Main.java
+++ src/main/java/io/schinzel/samples/script_file/Main.java
@@ -2,15 +2,14 @@ package io.schinzel.samples.script_file;
 
 import com.atexpose.AtExpose;
 
+/**
+ * In this sample, a script file is loaded. The script file contains a set of requests to execute.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-        AtExpose atExpose = AtExpose.create();
-        atExpose.loadScriptFile("script_file/scriptfile.txt");
-        //Start command line interface
-        atExpose.startCLI();
-
-
-
+        AtExpose.create()
+                .loadScriptFile("script_file/scriptfile.txt");
     }
 }
