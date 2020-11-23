3b9a5668aa8e6390682255fc222774b669ca6892
Henrik Schinzel <henrik@schinzel.io>
2020-11-23 08:26:30 +0100

    Demo added

--- src/main/java/io/schinzel/samples/Sample9_Cookies.java
+++ src/main/java/io/schinzel/samples/Sample9_Cookies.java
@@ -1,7 +1,27 @@
 package io.schinzel.samples;
 
+import com.atexpose.AtExpose;
+import io.schinzel.samples.auxiliary.MyClass;
+
 /**
+ * <p>
  * The purpose of this sample is to show how cookies are set and read.
+ * </p>
+ * <pre>
+ * 1) Call below URL to set a cookie.
+ * http://127.0.0.1:5555/api/setCookie
+ * 2) Check in the browser that the cookie was set.
+ * 3) Call below URL to read the cookie that was part of the request.
+ * It's value will be written to standard out.
+ * http://127.0.0.1:5555/api/readCookie
+ * </pre>
  */
 public class Sample9_Cookies {
+    public static void main(String[] args) {
+        AtExpose.create()
+                //Expose static methods in a class
+                .expose(MyClass.class)
+                //Start a web server
+                .startWebServer();
+    }
 }
--- src/main/java/io/schinzel/samples/auxiliary/MyClass.java
+++ src/main/java/io/schinzel/samples/auxiliary/MyClass.java
@@ -1,6 +1,11 @@
 package io.schinzel.samples.auxiliary;
 
 import com.atexpose.Expose;
+import com.atexpose.util.web_cookie.ResponseCookie;
+import com.atexpose.util.web_cookie.WebCookieHandler;
+import io.schinzel.basicutils.RandomUtil;
+
+import java.time.Instant;
 
 /**
  * The purpose of this class it to hold sample static methods that will be exposed.
@@ -26,5 +31,28 @@ public class MyClass {
     )
     public static String doEcho(String str) {
         return "Echo: " + str;
+    }
+
+
+    //------------------------------------------------------------------------
+    // Cookie demo
+    //------------------------------------------------------------------------
+
+    @Expose
+    public static void setCookie() {
+        final ResponseCookie responseCookie = ResponseCookie.builder()
+                .name("my_cookie")
+                .value(RandomUtil.getRandomString(5))
+                .expires(Instant.now().plusSeconds(20 * 60))
+                .build();
+        WebCookieHandler.addResponseCookie(responseCookie);
+    }
+
+    @Expose
+    public static void readCookie() {
+        final String cookieValue = WebCookieHandler
+                .getRequestCookie("my_cookie")
+                .getValue();
+        System.out.println("Value of cookie part of the request '" + cookieValue + "'");
     }
 }
