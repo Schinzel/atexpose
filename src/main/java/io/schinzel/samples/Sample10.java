144e4f69dfa87229e90131e4626a03b38cc8691a
Henrik Schinzel <henrik@schinzel.io>
2020-11-23 14:30:45 +0100

    Sample code

--- src/main/java/io/schinzel/samples/Sample10.java
+++ src/main/java/io/schinzel/samples/Sample10.java
@@ -10,7 +10,8 @@ import io.schinzel.samples.sample10.Position;
 import io.schinzel.samples.sample10.SampleAPI;
 
 /**
- * The purpose of this sample is to show how to use custom classes and enums with @expose.
+ * The purpose of this sample is to show how to use custom arguments with custom classes and
+ * enums in @expose.
  * <p>
  * Load page: http://127.0.0.1:5555/
  */
@@ -18,19 +19,24 @@ public class Sample10 {
 
     public static void main(String[] args) {
         AtExpose.create()
+                // Add new data types
                 .addDataType(Person.class)
                 .addDataType(Position.class)
+                // Create a an argument "manager" that is of data type "Person"
                 .addArgument(Argument.builder()
                         .name("manager")
                         .dataType(new ClassDT<>(Person.class))
                         .description("A manager")
                         .build())
+                // Create a an argument "position" that is of data type "Position"
                 .addArgument(Argument.builder()
                         .name("position")
                         .dataType(new ClassDT<>(Position.class))
                         .defaultValue('"' + Position.FIRST.name() + '"')
                         .description("The position")
                         .build())
+                // Create an argument "sample_arg" that is of data type "String"
+                // was a reg ex that defines which chars are allowed.
                 .addArgument(Argument.builder()
                         .name("sample_arg")
                         .dataType(DataTypeEnum.STRING.getDataType())
@@ -38,8 +44,12 @@ public class Sample10 {
                         .defaultValue("default-value")
                         .description("Description of the sample argument")
                         .build())
+                // Expose an API
                 .expose(SampleAPI.class)
+                // Create a client for calling the server. Person and position will be translated
+                // to JavaScript and will be available as class and object
                 .generateJavaScriptClient("src/main/resources/web/sample10/ServerCaller.js")
+                // Start a web server
                 .startWebServer("web/sample10");
         System.out.println("System started!");
     }
