23b138ed60361689a7c482ab11d9ea59012082e1
Henrik Schinzel <henrik@schinzel.io>
2021-03-12 06:37:44 +0100

    Add error class to error message

--- src/main/java/com/atexpose/dispatcher/Dispatcher.java
+++ src/main/java/com/atexpose/dispatcher/Dispatcher.java
@@ -206,6 +206,8 @@ public class Dispatcher implements Runnable, IDispatcher {
                         ? e.getCause().getMessage()
                         : e.getMessage();
                 if (errorMessage == null) errorMessage = "";
+                String errorClassName = e.getClass().getSimpleName();
+                errorMessage = errorClassName + " " + errorMessage;
                 ImmutableMap<String, String> requestExceptionInfo = (request.isFileRequest())
                         ? ImmutableMap.<String, String>builder()
                         .put("error_message", errorMessage)
